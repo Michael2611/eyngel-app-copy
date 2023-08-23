c0a772e87788c06bd7a9da03cc9d6a72cf81f78b
angel <angel_yesid26@outlook.com>
2023-08-23 16:29:29 -0500

    1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request; // ajuste nuevo
 use Illuminate\Support\Facades\Auth;
 
-
 class LoginController extends Controller
 {
     /*
@@ -45,7 +44,7 @@ class LoginController extends Controller
     {
         $this->validateLogin($request);
 
-        $remember = $request->filled('remember'); // "Recuérdame" está marcado
+        $remember = $request->filled('remember'); //  "Recuérdame" está marcado
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
             return $this->sendLoginResponse($request);
